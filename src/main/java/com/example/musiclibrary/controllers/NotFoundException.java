8241c3ebf25a0b70925ad15fc4c49e6c849dcf44
Ilya Karasev <uvp112.karasev.i@mail.ru>
2025-05-04 01:20:23 +0300

    Revert "Initial commit"
    This reverts commit c223e88db02c5d61c22b3b59975e2dfad7441c8e.

--- src/main/java/com/example/musiclibrary/controllers/NotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.musiclibrary.controllers;
-class NotFoundException extends RuntimeException {
-    NotFoundException(String obj) {
-        super("Could not find " + obj);
-    }
-}
